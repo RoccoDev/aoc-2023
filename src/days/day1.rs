65454df97699b8524534f8100424c934637aa36f
RoccoDev <hey@rocco.dev>
2023-12-01 17:53:29 +0100

    days: improve day1 solution

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -2,40 +2,35 @@ use itertools::Itertools;
 
 // We don't use replace over here
 #[derive(Clone, Copy, Debug)]
-struct Parser {
-    state: ParserState,
-    sub: usize,
+struct Parser;
+
+#[derive(Clone, Copy, Debug)]
+struct ParserState {
+    token: ParserToken,
+    index: usize,
 }
 
 #[derive(Debug)]
 enum ParserResult {
     MatchAlpha(u32),
     MatchNumeric(u32),
-    Continue(Parser),
+    Continue(ParserState),
     Skip,
 }
 
 #[derive(Clone, Copy, Debug)]
-enum ParserState {
-    Unknown,
-    // with sub: 0: 'o', 1: 'on'
-    One,
-    // 0: 't', 1: 'tw'
-    Two,
-    // 0: 'th', 1: 'thr', 2: 'thre'
-    Three,
-    // 0: 'f', 1: 'fo', 2: 'fou'
-    Four,
-    // 0: 'fi', 1: 'fiv'
-    Five,
-    // 0: 's', 1: 'si'
-    Six,
-    // 0: 'se', 1: 'sev', 2: 'seve'
-    Seven,
-    // 0: 'e', 1: 'ei', 2: 'eig', 3: 'eigh'
-    Eight,
-    // 0: 'n', 1: 'ni', 2: 'nin'
-    Nine,
+#[repr(u32)]
+enum ParserToken {
+    Unknown = 0,
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9,
 }
 
 #[aoc(day1, part1)]
@@ -46,7 +41,7 @@ pub fn part1(input: &str) -> u32 {
             let (first, last) = l
                 .chars()
                 .enumerate()
-                .filter(|(_, c)| c.is_numeric())
+                .filter(|(_, c)| c.is_ascii_digit())
                 .minmax_by_key(|(i, _)| *i)
                 .into_option()
                 .unwrap();
@@ -84,23 +79,16 @@ pub fn part2(input: &str) -> u32 {
 }
 
 impl Parser {
-    pub fn new() -> Self {
-        Self {
-            state: ParserState::Unknown,
-            sub: 0,
-        }
-    }
-
     pub fn parse_complete(st: &[u8]) -> (usize, Option<u32>) {
-        let mut parser = Self::new();
+        let mut state = ParserState::new(ParserToken::Unknown, 0);
         let mut consumed = 0;
         while consumed < st.len() {
-            match parser.consume(st[consumed]) {
+            match Parser::consume(state, st[consumed]) {
                 // Alpha strings can overlap
                 ParserResult::MatchAlpha(n) => return (consumed, Some(n)),
                 // Numeric strings can't
                 ParserResult::MatchNumeric(n) => return (consumed + 1, Some(n)),
-                ParserResult::Continue(p) => parser = p,
+                ParserResult::Continue(p) => state = p,
                 // We only skip on alpha strings that can overlap
                 ParserResult::Skip => return (consumed, None),
             }
@@ -109,78 +97,97 @@ impl Parser {
         (st.len(), None)
     }
 
-    fn consume(self, ch: u8) -> ParserResult {
-        use ParserState::*;
+    fn consume(mut state: ParserState, ch: u8) -> ParserResult {
+        use ParserToken::*;
 
-        let (next, len) = match (self.state, self.sub, char::from(ch)) {
-            (_, _, ch) if ch.is_numeric() => {
-                return ParserResult::MatchNumeric(ch.to_digit(10).unwrap())
-            }
+        let ch = char::from(ch);
+        if ch.is_ascii_digit() {
+            return ParserResult::MatchNumeric(ch.to_digit(10).unwrap());
+        }
 
-            (Unknown, _, 'o') => (One, 0),
-            (Unknown, _, 't') => (Two, 0),
-            (Unknown, _, 'f') => (Four, 0),
-            (Unknown, _, 's') => (Six, 0),
-            (Unknown, _, 'e') => (Eight, 0),
-            (Unknown, _, 'n') => (Nine, 0),
-            (Unknown, _, _) => (Unknown, 0),
+        if let Unknown = state.token {
+            return ParserResult::Continue(ParserState::new(
+                match ch {
+                    'o' => One,
+                    't' => Two,
+                    'f' => Four,
+                    's' => Six,
+                    'e' => Eight,
+                    'n' => Nine,
+                    _ => Unknown,
+                },
+                1,
+            ));
+        }
 
-            (One, 0, 'n') => (One, 1),
-            (One, 1, 'e') => return ParserResult::MatchAlpha(1),
-            (Two, 0, 'w') => (Two, 1),
-            (Two, 1, 'o') => return ParserResult::MatchAlpha(2),
-            (Two, 0, 'h') => (Three, 0),
-            (Three, 0, 'r') => (Three, 1),
-            (Three, 1, 'e') => (Three, 2),
-            (Three, 2, 'e') => return ParserResult::MatchAlpha(3),
-            (Four, 0, 'o') => (Four, 1),
-            (Four, 1, 'u') => (Four, 2),
-            (Four, 2, 'r') => return ParserResult::MatchAlpha(4),
-            (Four, 0, 'i') => (Five, 0),
-            (Five, 0, 'v') => (Five, 1),
-            (Five, 1, 'e') => return ParserResult::MatchAlpha(5),
-            (Six, 0, 'i') => (Six, 1),
-            (Six, 1, 'x') => return ParserResult::MatchAlpha(6),
-            (Six, 0, 'e') => (Seven, 0),
-            (Seven, 0, 'v') => (Seven, 1),
-            (Seven, 1, 'e') => (Seven, 2),
-            (Seven, 2, 'n') => return ParserResult::MatchAlpha(7),
-            (Eight, 1, 'g') => (Eight, 2),
-            (Eight, 2, 'h') => (Eight, 3),
-            (Eight, 3, 't') => return ParserResult::MatchAlpha(8),
-            (Nine, 0, 'i') => (Nine, 1),
-            (Nine, 1, 'n') => (Nine, 2),
-            (Nine, 2, 'e') => return ParserResult::MatchAlpha(9),
+        let current_char = state.current_char();
 
-            // When part of a number can be the start of another
-            (_, _, 'i') if self.ends_with_e() => (Eight, 1),
-            (_, _, 'i') if self.ends_with_n() => (Nine, 1),
-            (Four, 1, 'n') /* o */ => (One, 1),
+        if !state.next(ch) {
+            // Branching states
+            return ParserResult::Continue(match (current_char, ch) {
+                // When part of a number can be the start of another
+                ('e', 'i') => ParserState::new(Eight, 2),
+                ('n', 'i') => ParserState::new(Nine, 2),
+                ('o', 'n') => ParserState::new(One, 2),
 
-            (_, _, _) => return ParserResult::Skip,
-        };
+                // Branch Seven, Five, Three (from Six, Four, Two)
+                ('s', 'e') => ParserState::new(Seven, 2),
+                ('f', 'i') => ParserState::new(Five, 2),
+                ('t', 'h') => ParserState::new(Three, 2),
+                _ => return ParserResult::Skip,
+            });
+        }
 
-        ParserResult::Continue(Parser {
-            state: next,
-            sub: len,
-        })
+        if state.is_full_match() {
+            return ParserResult::MatchAlpha(state.token.result());
+        }
+
+        ParserResult::Continue(state)
+    }
+}
+
+impl ParserState {
+    pub fn new(token: ParserToken, index: usize) -> Self {
+        Self { token, index }
     }
 
-    fn ends_with_e(&self) -> bool {
-        use ParserState::*;
-
-        // see above comments
-        matches!(
-            (self.state, self.sub),
-            (Eight, 0) | (Seven, 0) | (Seven, 2) | (Three, 2)
-        )
+    pub fn next(&mut self, next_char: char) -> bool {
+        if char::from(self.token.pattern()[self.index]) != next_char {
+            return false;
+        }
+        self.index += 1;
+        true
     }
 
-    fn ends_with_n(&self) -> bool {
-        use ParserState::*;
+    pub fn is_full_match(&self) -> bool {
+        self.index >= self.token.pattern().len()
+    }
 
-        // see above comments
-        matches!((self.state, self.sub), (Nine, 0) | (Nine, 2) | (One, 1))
+    fn current_char(&self) -> char {
+        char::from(self.token.pattern()[self.index - 1])
+    }
+}
+
+impl ParserToken {
+    const fn result(self) -> u32 {
+        self as u32
+    }
+
+    const fn pattern(self) -> &'static [u8] {
+        use ParserToken::*;
+        match self {
+            Unknown => "",
+            One => "one",
+            Two => "two",
+            Three => "three",
+            Four => "four",
+            Five => "five",
+            Six => "six",
+            Seven => "seven",
+            Eight => "eight",
+            Nine => "nine",
+        }
+        .as_bytes()
     }
 }
 
@@ -212,5 +219,9 @@ zoneight234
     #[test]
     fn part2_edge() {
         assert_eq!(part2("xgkfonethreexnlcptbgxhnine4fivetwosix"), 16);
+        assert_eq!(part2("sevenine"), 79);
+        assert_eq!(part2("oneight"), 18);
+        assert_eq!(part2("threeeeight"), 38);
+        assert_eq!(part2("ninine2"), 92);
     }
 }
